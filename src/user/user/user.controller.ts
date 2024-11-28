7991963328c4bbd6416964e60d68c8fa5c55cf27
vincent exelcio <vincent.celcio@gmail.com>
2024-11-28 11:35:35 +0700

    coba request dan response

---
+++ src/user/user/user.controller.ts
@@ -0,0 +1,69 @@
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  HttpRedirectResponse,
+  Param,
+  Post,
+  Query,
+  Redirect,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Controller('/api/users')
+export class UserController {
+  @Get('sample-response')
+  @Header('Content-Type', 'Application/json')
+  @HttpCode(200)
+  sampleResponse(): Record<string, string> {
+    return {
+      data: 'Hello JSON',
+    };
+  }
+
+  @Get('/set-cookie')
+  setCookie(@Query('name') name: string, @Res() response: Response) {
+    response.cookie('name', name);
+    response.status(200).send('berhasil menambahkan cookie');
+  }
+
+  @Get('/get-cookie')
+  getCookie(@Req() request: Request): string {
+    return request.cookies['name'];
+  }
+
+  @Get('hello')
+  async sayHello(
+    @Query('first_name') firstName: string,
+    @Query('last_name') lastName: string,
+  ): Promise<string> {
+    return `Hello ${firstName} ${lastName}`;
+  }
+
+  @Get('redirect')
+  @Redirect()
+  redirect(): HttpRedirectResponse {
+    return {
+      url: '/api/users/sample-response',
+      statusCode: 301,
+    };
+  }
+
+  @Get('/:id')
+  getById(@Param('id') id: string): string {
+    return `GET ${id}`;
+  }
+
+  @Post()
+  post(): string {
+    return 'POST';
+  }
+
+  @Get('/sample')
+  get(): string {
+    return 'GET';
+  }
+}
