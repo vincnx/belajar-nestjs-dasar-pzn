a15461ef2fc6efa683c16b821b043ef25cd15ff3
vincent exelcio <vincent.celcio@gmail.com>
2024-11-28 12:02:16 +0700

    unit testing

--- src/user/user/user.controller.spec.ts
+++ src/user/user/user.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import * as httpMock from 'node-mocks-http';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -12,7 +13,19 @@ describe('UserController', () => {
     controller = module.get<UserController>(UserController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should be say hello with first name and last name', async () => {
+    const response = await controller.sayHello('vincent', 'exelcio');
+    expect(response).toBe('Hello vincent exelcio');
+  });
+
+  it('should can view template', async () => {
+    const response = httpMock.createResponse();
+    controller.viewHello('vincent', response);
+
+    expect(response._getRenderView()).toBe('index.html');
+    expect(response._getRenderData()).toEqual({
+      name: 'vincent',
+      title: 'template engine',
+    });
   });
 });
